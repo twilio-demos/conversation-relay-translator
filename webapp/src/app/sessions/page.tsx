c692ecbfcd95f69ed7840eb0dcd6b3fe2a7e1925
Jayden Pyles <jpyles@twilio.com>
2026-02-17 09:25:17 -0600

    fix: add force dynamic

--- webapp/src/app/sessions/page.tsx
+++ webapp/src/app/sessions/page.tsx
@@ -1,6 +1,8 @@
 import { listSessions } from "@/lib/dynamodb";
 import ClientSessionsPage from "../components/pages/sessions";
 
+export const dynamic = "force-dynamic";
+
 export default async function SessionsPage() {
   const sessions = await listSessions();
 
